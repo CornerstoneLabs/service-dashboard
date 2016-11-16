84693fd9ca26aad0f37eaa1ee253334336cf7fda
Adam Auckland <adamauckland@gmail.com>
2016-11-16 16:41:23 +0000

    free

--- dashboard/rules/memory-check/rule-low-memory.js
+++ dashboard/rules/memory-check/rule-low-memory.js
@@ -12,7 +12,7 @@ module.exports = function evaluateMemory(data, schedule, monitor) {
 	if (monitor.fabricCommand === 'status__memory_free') {
 		try {
 			var parsedData = memoryParser(data);
-			var available = parseInt(parsedData.available);
+			var available = parseInt(parsedData.available || parsedData.free);
 			var total = parseInt(parsedData.total);
 			var percentage = parseInt((parseFloat(available) / parseFloat(total)) * 100);
 			var quarter = (total / 4);
