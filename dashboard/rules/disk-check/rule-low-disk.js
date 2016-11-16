380058319c3df9c647426f8ba5d232c147063391
Adam Auckland <adamauckland@gmail.com>
2016-11-16 11:49:26 +0000

    change

--- dashboard/rules/disk-check/rule-low-disk.js
+++ dashboard/rules/disk-check/rule-low-disk.js
@@ -2,10 +2,22 @@ module.exports = function evaluateDisk(data, schedule, monitor) {
 	if (monitor.fabricCommand === 'status__disk') {
 		try {
 			data.forEach(function (disk) {
-				data.messages.push(`${disk.name}: ${disk.use_percent}% of ${disk.size}`);
+				let name = disk.name;
+				let use_percent = disk.use_percent;
+				let size = disk.size + 'KB';
+
+				if (disk.size > 1024) {
+					size = disk.size + 'MB';
+				}
+
+				if (disk.size > 1024*1024) {
+					size = disk.size + 'GB';
+				}
+
+				data.messages.push(`${name}: ${use_percent}% of ${size}`);
 			});
 		} catch (e) {
-
+			console.log(e);
 		}
 	}
 };
